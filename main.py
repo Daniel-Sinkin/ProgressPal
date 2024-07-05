fa4db9fb554062a007d889863d5552a144ab00cf
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-07-06 01:26:01 +0200

    Improved main and implemented the serialization and deserialization

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-from src.constants import Rarity
 from src.player import Player
 
 
 def main():
     player = Player("Daniel")
-    player.habit_journaling(Rarity.Nothing)
-    player.habit_journaling(Rarity.Common)
-    player.habit_journaling(Rarity.Uncommon)
-    player.habit_journaling(Rarity.Rare)
-    player.habit_journaling(Rarity.VeryRare)
+    for rarity in player.pull_rarities(1):
+        print(f"Pulled rarity: {rarity}")
+        player.habit_journaling(rarity)
+        print()
+    print(player)
+
+    # Serialize to dictionary
+    serialized_data = player.serialize()
+
+    # Serialize to JSON
+    json_data = player.to_json()
+
+    # Deserialize from dictionary
+    new_player = Player.deserialize(serialized_data)
+
+    # Deserialize from JSON
+    another_player = Player.from_json(json_data)
+
+    print(f"{new_player=}")
+    print(f"{another_player=}")
 
 
 if __name__ == "__main__":
